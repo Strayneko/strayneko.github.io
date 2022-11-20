cf77b5147c9f37c031eed25ec00d453ef349448d
Strayneko <bayurendi10@gmail.com>
2022-11-20 22:48:21 +0700

    :sparkles: add more interactive element

--- js/script.js
+++ js/script.js
@@ -11,13 +11,47 @@ hamburgerBtn.addEventListener("click", () => {
   document.querySelector(".navbar-menu").classList.toggle("hidden");
 });
 
-/*
-* ===============
-* Navbar scroll
-* ===============
-*/
 window.onscroll = () => {
+  /*
+  * ===============
+  * Navbar scroll
+  * ===============
+  */
+
   window.pageYOffset > el("#hero").offsetTop
     ? elClass("#header").add("navbar-fixed")
     : elClass("#header").remove("navbar-fixed");
+
+  /*
+* ===============
+* Scroll spy
+* ===============
+*/
+
+  if (window.pageYOffset > el("#always-online").offsetTop) {
+    el('a[href="#always-online"]').classList.add("active");
+    el('a[href="#overview"]').classList.remove("active");
+  } else {
+    el('a[href="#overview"]').classList.add("active");
+    el('a[href="#always-online"]').classList.remove("active");
+  }
+  if (window.pageYOffset > el("#pricing").offsetTop) {
+    el('a[href="#pricing"]').classList.add("active");
+    el('a[href="#always-online"]').classList.remove("active");
+  } else {
+    el('a[href="#pricing"]').classList.remove("active");
+  }
+  if (window.pageYOffset > el("#feedback").offsetTop) {
+    el('a[href="#feedback"]').classList.add("active");
+    el('a[href="#pricing"]').classList.remove("active");
+  } else {
+    el('a[href="#feedback"]').classList.remove("active");
+  }
 };
+
+/*
+* ===============
+* Carousel
+* ===============
+*/
+el(".carousel-wrapper").scrollLeft = window.innerWidth / 20;
